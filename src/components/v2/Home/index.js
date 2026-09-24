a41674889f4882f0ea3bddc51251f53077984324
Oluwafunso Oluyole-Balogun <ooluyole-balogun@datastealth.io>
2026-09-24 11:45:12 -0400

    feat: updates years of experience

--- src/components/v2/Home/index.js
+++ src/components/v2/Home/index.js
@@ -5,6 +5,8 @@ import Typography from "../common/Typography";
 import "./index.css";
 
 
+const experienceYears = new Date().getFullYear() - 2017;
+
 export default function Home () {
     return (
         <div id="home">
@@ -16,7 +18,7 @@ export default function Home () {
                 margin="20px 0"
             />
             <Typography
-                text="I'm a full-stack software engineer with 7+ years of hands-on professional experience in software development, process improvement, on-time delivery of projects, with strong front-end and backend development experience. Self-motivated & committed individual who loves challenges, quick to learn, and appreciates feedback."
+                text={`I'm a full-stack software engineer with ${experienceYears}+ years of hands-on professional experience in software development, process improvement, on-time delivery of projects, with strong front-end and backend development experience. Self-motivated & committed individual who loves challenges, quick to learn, and appreciates feedback.`}
                 textAlign="left"
                 fontWeight="200"
                 margin="20px 0 0 0"
